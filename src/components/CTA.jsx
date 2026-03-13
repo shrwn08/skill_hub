72ed7e9dfcee20e28464a2e14dc0ef6c80fbee0d
Shrawan <shrwnkumar08@gmail.com>
2026-03-14 00:03:41 +0530

    feat: add cta section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="bg-(--soil) text-center mx-4 sm:mx-8 md:mx-16 my-12 md:my-20 rounded-3xl py-12 sm:py-16 md:py-20 px-6 sm:px-8 md:px-10">
+
+      <h2 className="font-[Fraunces] text-3xl sm:text-3xl md:text-4xl lg:text-4xl text-(--cream) mb-4">
+        Your business idea is
+        <em className="italic text-(--gold)"> waiting for you.</em>
+      </h2>
+
+      <p className="text-(--cream)/70 text-base sm:text-base md:text-lg mb-8">
+        Join 2,400+ entrepreneurs who already started.
+      </p>
+
+      <button className="bg-(--gold) text-(--soil) px-6 sm:px-8 md:px-10 py-3 sm:py-4 md:py-4 rounded-full text-sm sm:text-base md:text-base">
+        Take the Free Skills Quiz →
+      </button>
+
+    </div>
   )
 }
 
-export default CTA
+export default CTA;
--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Stats = () => {
   return (
-    <div>stats</div>
+    <div className="bg-(--soil) text-(--cream) grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 px-6 md:px-10 lg:px-16 py-12 text-center">
+
+      <div>
+        <h3 className="font-[Fraunces] text-3xl md:text-4xl text-(--gold)">2400+</h3>
+        <p className="text-sm opacity-60">Entrepreneurs enrolled</p>
+      </div>
+
+      <div>
+        <h3 className="font-[Fraunces] text-3xl md:text-4xl text-(--gold)">180+</h3>
+        <p className="text-sm opacity-60">Verified mentors</p>
+      </div>
+
+      <div>
+        <h3 className="font-[Fraunces] text-3xl md:text-4xl text-(--gold)">60+</h3>
+        <p className="text-sm opacity-60">Business templates</p>
+      </div>
+
+      <div>
+        <h3 className="font-[Fraunces] text-3xl md:text-4xl text-(--gold)">92%</h3>
+        <p className="text-sm opacity-60">Completion rate</p>
+      </div>
+
+    </div>
   )
 }
 
-export default Stats
+export default Stats;
